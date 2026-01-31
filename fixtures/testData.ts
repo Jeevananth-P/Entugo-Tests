ac7bca475277635a630d690aec20fe1222bab15f
Jeevananth-P <106537997+Jeevananth-P@users.noreply.github.com>
2026-01-31 12:17:19 +0530

    Update testData.ts

--- fixtures/testData.ts
+++ fixtures/testData.ts
@@ -2,5 +2,15 @@ export const testData = {
   restaurant: {
     email: process.env.RESTAURANT_EMAIL!,
     password: process.env.RESTAURANT_PASSWORD!,
+    baseURL: process.env.CLIENT_URL!,  // https://client.entugo.com
   },
+  customer: {
+    name: "Jeeva",
+    code: "OV47LO2X",
+    email: process.env.CUSTOMER_EMAIL!,
+    password: process.env.CUSTOMER_PASSWORD!,
+    baseURL: process.env.CUSTOMER_URL!,    // https://customer.entugo.com
+  }
 };
+;
+
